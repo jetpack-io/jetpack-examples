0a7b43af06e231e9978c9589efa2b2f28aa5d2b3
Savil Srivastava <savil@cs.stanford.edu>
2021-12-13 11:56:26 -0800

    improvements

--- 06-is-it-down-monitor/app/jetpack_main.py
+++ 06-is-it-down-monitor/app/jetpack_main.py
@@ -1,4 +1,5 @@
 import asyncio
+import random
 import requests
 
 from dotenv import load_dotenv
@@ -9,50 +10,61 @@ from os import environ
 load_dotenv()
 app = FastAPI()
 
-@app.get("/")
-def hello_world():
-    return '<h1>Hello! You are currently in {0}</h1>'.format(
-        environ.get("FLASK_ENV"))
-
-@cron.repeat(cron.every(2).minutes)
+# This cronjob monitors the list of websites to see if any are down.
+@cron.repeat(cron.every(30).minutes)
 async def schedule_poll():
+  print("running cronjob schedule_poll")
   await poll_down_status_impl()
 
-@app.get("/poll_down_status")
-async def poll_down_status():
-  await poll_down_status_impl()
-
+@function
 async def poll_down_status_impl():
   print("starting poll_down_status")
+
+  # List of websites we care about. This could be set in a configuration store
+  # as well.
   websites = ["https://twitter.com", "https://cnn.com", "https://downdetector.com"]
+
   awaitables = []
   for website in websites:
     awaitables.append(check_if_down(website))
   
   is_downs = await asyncio.gather(*awaitables)
+  
+  notifications = []
   for is_website_down in is_downs:
-    print(f"checked if down for {website}")
+    website = is_website_down[0]
+    down_text = "down" if is_website_down[1] else "not down"
+    print(f"checked if down for {website} and it is {down_text}.")
     if is_website_down[1]:
-      website = is_website_down[0]
-      notify_slack(website)
+      notifications.append(notify_slack(website))
+  
+  await asyncio.gather(*notifications)
   return "done"
 
 @function
 async def check_if_down(website: str):
     r = requests.get(website)
-    return (website, True)
+    
+    # The following line is useful for debugging
+    # return (website, True if random.randint(0, 1) == 1 else False)
+
     return (website, r.status_code == requests.codes.ok)
 
-def notify_slack(website):
-    webhook_env_var_name = "SLACK_WEBHOOK_URL_SUFFIX"
-    url_suffix_value = environ.get(webhook_env_var_name)
-    if url_suffix_value == None:
+@function
+async def notify_slack(website):
+    webhook_env_var_name = "SLACK_WEBHOOK_URL"
+    url = environ.get(webhook_env_var_name)
+    if url == None:
       raise Exception("Please set an environment variable {webhook_env_var_name} for the slack incoming webhook URL.")
-    url = "https://hooks.slack.com/services/" + url_suffix_value
     payload = {"text":f"ALERT: {website} is down."}
     headers = {"content-type": "application/json", "Accept-Charset": "UTF-8"}
     response = requests.post(url, data=str(payload), headers=headers)
     print(f"Posted to slack. Response code is: {response.status_code}")
+
+# This endpoint is useful for debugging by triggering the cronjob on demand.
+@app.get("/poll_down_status")
+async def poll_down_status():
+  await poll_down_status_impl()
 
 if __name__ == "__main__":
     app.run()
