034ee07f308fcc2de2f96b66a6dd006fdccd1498
Lucille Hua <lucille.hua@gmail.com>
2022-06-09 16:57:26 -0700

    bump node sdk version

--- nodejs-redis-cache-example/src/cron.js
+++ nodejs-redis-cache-example/src/cron.js
@@ -1,9 +1,16 @@
-import { cron } from 'jetpack-io';
+import { cron, jetroutine } from 'jetpack-io';
 
 cron(
   'exampleCron',
+  '30 * * * *',
   () => {
     console.log('executing exampleCron!');
   },
-  '30 * * * *',
 );
+
+export const exampleJetroutine = jetroutine(
+  'exampleJetroutine',
+  () => {
+    console.log('hello from example jetroutine!');
+  }
+)
